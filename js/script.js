87be0d86289b332a7172ce61ba9e4360811a912c
sandernilsen <98823397+SanderNilsen@users.noreply.github.com>
2023-10-21 12:51:45 +0200

    favicon
    cleanup

--- js/script.js
+++ js/script.js
@@ -17,10 +17,12 @@ function updateSlider() {
 
 setInterval(nextSlide, 5000);
 
-const hamburgerMenu = document.querySelector('.hamburger-menu');
-const navLinks = document.querySelector('.nav-links');
+//Hamburger-menu
 
-hamburgerMenu.addEventListener('click', () => {
-    hamburgerMenu.classList.toggle('active');
-    navLinks.classList.toggle('active');
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const navLinks = document.querySelector(".nav-links");
+
+hamburgerMenu.addEventListener("click", () => {
+    hamburgerMenu.classList.toggle("active");
+    navLinks.classList.toggle("active");
 });
